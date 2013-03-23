bad8051c2c50b8c037fd5596e876faef29ca1ff8
mkiken <mori9@is.titech.ac.jp>
2013-03-23 23:57:58 +0900

    init.el modified.

--- emacs/init.el
+++ emacs/init.el
@@ -120,6 +120,33 @@
 ;; C-S-kに設定
 (global-set-key (kbd "C-S-k") 'backward-kill-line)
 
+;; http://d.hatena.ne.jp/gifnksm/20100131/1264956220
+(defun beginning-of-visual-indented-line (current-point)
+  "インデント文字を飛ばした行頭に戻る。ただし、ポイントから行頭までの間にインデント文 字しかない場合は、行頭に戻る。"
+  (interactive "d")
+  (let ((vhead-pos (save-excursion (progn (beginning-of-visual-line) (point))))
+        (head-pos (save-excursion (progn (beginning-of-line) (point)))))
+    (cond
+     ;; 物理行の1行目にいる場合
+     ((eq vhead-pos head-pos)
+      (if (string-match
+           "^[ \t]+$"
+           (buffer-substring-no-properties vhead-pos current-point))
+          (beginning-of-visual-line)
+        (back-to-indentation)))
+     ;; 物理行の2行目以降の先頭にいる場合
+     ((eq vhead-pos current-point)
+      (backward-char)
+      (beginning-of-visual-indented-line (point)))
+     ;; 物理行の2行目以降の途中にいる場合
+     (t (beginning-of-visual-line)))))
+
+(global-set-key "\C-a" 'beginning-of-visual-indented-line)
+(global-set-key "\C-e" 'end-of-visual-line)
+
+;; C-kで行全体を削除
+(setq kill-whole-line t)
+
 ;;http://www.bookshelf.jp/soft/meadow_23.html#SEC231
 (ffap-bindings)
 
