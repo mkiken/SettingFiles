e14e36bb5531197772b02ada2a284caf6c8150db
mkiken <mkiken0@gmail.com>
2014-05-19 02:41:03 +0900

    emacのタブUIを大幅に変更

---
+++ kr4mb-import.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cli=/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli
+
+$cli set repeat.wait 23
+/bin/echo -n .
+$cli set repeat.initial_wait 200
+/bin/echo -n .
+/bin/echo
