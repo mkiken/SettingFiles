5a461d3a4df8fd16dfa5a89faf6ac2200842bad5
mkiken <mkiken0@gmail.com>
2014-06-13 21:12:08 +0900

    こまごました設定

--- mac/kr4mb-import.sh
+++ mac/kr4mb-import.sh
@@ -2,8 +2,8 @@
 
 cli=/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli
 
-$cli set repeat.wait 23
+$cli set repeat.wait 3
 /bin/echo -n .
-$cli set repeat.initial_wait 200
+$cli set repeat.initial_wait 100
 /bin/echo -n .
 /bin/echo
