efa4c13ccec85022606a28a450addfa18cd46345
mkiken <mkiken0@gmail.com>
2023-02-20 22:07:49 +0900

    [vim] .spacemacsのsyntaxセット

--- vimfiles/nvchad/custom/init.lua
+++ vimfiles/nvchad/custom/init.lua
@@ -1,3 +1,11 @@
 -- クリップボードはvimとOSは分ける
 vim.opt.clipboard = ""
 vim.opt.gdefault = true
+
+-- autocmds
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd('BufEnter', {
+  pattern = '.spacemacs',
+  command = "set filetype=lisp"
+})
