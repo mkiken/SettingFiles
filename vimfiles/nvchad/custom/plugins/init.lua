73bfecd7ea03327635f5eb780975bb27919e9916
mkiken <mkiken0@gmail.com>
2023-03-06 01:58:47 +0900

    [vim] surroundとshade追加
    * nvchadのupdateも追加

--- vimfiles/nvchad/custom/plugins/init.lua
+++ vimfiles/nvchad/custom/plugins/init.lua
@@ -10,4 +10,26 @@ return {
       },
     },
   },
+
+  ["kylechui/nvim-surround"] = {
+    tag = "*",
+    config = function()
+      require("nvim-surround").setup()
+    end,
+  },
+
+  -- dim inactive windows
+  ["sunjon/shade.nvim"] = {
+    config = function()
+      require("shade").setup({
+        overlay_opacity = 50,
+        opacity_step = 1,
+        keys = {
+          -- brightness_up    = '<C-Up>',
+          -- brightness_down  = '<C-Down>',
+          -- toggle           = '<Leader>s',
+        }
+      })
+    end,
+  },
 }
