201fa01c941e1e417d65053bffa2e8b7196735b4
mkiken <mkiken0@gmail.com>
2024-11-28 00:50:55 +0900

    [vim] プラグイン更新

--- vimfiles/nvim/lua/plugins/mini-indentscope.lua
+++ vimfiles/nvim/lua/plugins/mini-indentscope.lua
@@ -1,2 +1,6 @@
-return { 'echasnovski/mini.indentscope', version = false }
+return { 'echasnovski/mini.indentscope', version = false,
+config = function()
+  require('mini.indentscope').setup()
+end
+}
 
