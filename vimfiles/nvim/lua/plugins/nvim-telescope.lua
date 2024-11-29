f2a90e4b9dc4e77253b2e2115278c0b7f9487c46
mkiken <mkiken0@gmail.com>
2024-11-30 00:27:51 +0900

    [vim] telescope

---
+++ vimfiles/nvim/lua/plugins/nvim-telescope.lua
@@ -0,0 +1,11 @@
+local builtin = require('telescope.builtin')
+return {
+  'nvim-telescope/telescope.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  keys = {
+            { "<leader>ff", function() builtin.find_files() end, desc="Telescope find files" },
+            { "<leader>fg", function() builtin.live_grep() end, desc="Telescope live grep" },
+            { "<leader>fb", function() builtin.buffers() end, desc="Telescope buffers" },
+            { "<leader>fn", function() builtin.help_tags() end, desc="Telescope help tags" }
+    }
+}
