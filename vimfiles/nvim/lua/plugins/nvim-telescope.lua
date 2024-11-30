f6b845802d76f65a883db6d0bc9f6a464a3fdd53
mkiken <mkiken0@gmail.com>
2024-11-30 21:15:52 +0900

    [vim] nvim起動でtelescopeエラー

--- vimfiles/nvim/lua/plugins/nvim-telescope.lua
+++ vimfiles/nvim/lua/plugins/nvim-telescope.lua
@@ -1,15 +1,13 @@
-local telescope = require('telescope')
-local builtin = require('telescope.builtin')
 return {
   'nvim-telescope/telescope.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
-    { "<leader>ff", function() builtin.find_files() end, desc="Telescope find files" },
-    { "<leader>fg", function() builtin.live_grep() end, desc="Telescope live grep" },
-    { "<leader>fb", function() builtin.buffers() end, desc="Telescope buffers" },
-    { "<leader>fn", function() builtin.help_tags() end, desc="Telescope help tags" }
+    { "<leader>ff", function() require('telescope.builtin').find_files() end, desc="Telescope find files" },
+    { "<leader>fg", function() require('telescope.builtin').live_grep() end, desc="Telescope live grep" },
+    { "<leader>fb", function() require('telescope.builtin').buffers() end, desc="Telescope buffers" },
+    { "<leader>fn", function() require('telescope.builtin').help_tags() end, desc="Telescope help tags" }
   },
   config = function()
-    telescope.load_extension('fzf')
+    require('telescope').load_extension('fzf')
   end
 }
