2306c56f76ab8d252c86aa77d85770befd8841fa
mkiken <mkiken0@gmail.com>
2024-12-18 00:42:34 +0900

    [vim] nvim-treesitter-context

--- vimfiles/nvim/lua/plugins/nvim-hlslens.lua
+++ vimfiles/nvim/lua/plugins/nvim-hlslens.lua
@@ -7,5 +7,13 @@ return {
         nearest_only = true,
       })
     })
+    vim.keymap.set({'n', 'x'}, '<leader>L', function()
+      vim.schedule(function()
+        if require('hlslens').exportLastSearchToQuickfix() then
+          vim.cmd('cw')
+        end
+      end)
+      return ':noh<CR>'
+    end, {expr = true})
   end,
 }
