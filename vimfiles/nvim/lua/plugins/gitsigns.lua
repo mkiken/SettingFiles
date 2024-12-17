0eeacfa5b95dd44a1a817560d558879f65aa8f3f
mkiken <mkiken0@gmail.com>
2024-12-17 21:40:34 +0900

    [vim] scrollbarプラグイン

--- vimfiles/nvim/lua/plugins/gitsigns.lua
+++ vimfiles/nvim/lua/plugins/gitsigns.lua
@@ -1,4 +1,7 @@
 return {
     'lewis6991/gitsigns.nvim',
-    config = true
+    config = function()
+      require('gitsigns').setup()
+      require("scrollbar.handlers.gitsigns").setup()
+    end
   }
