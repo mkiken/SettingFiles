d41f9a14656ed5fb7a9194808f0f6e7edc3b2652
mkiken <mkiken0@gmail.com>
2024-12-18 01:10:13 +0900

    [vim] hlchunk

---
+++ vimfiles/nvim/lua/plugins/hlchunk.lua
@@ -0,0 +1,15 @@
+return {
+  "shellRaining/hlchunk.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("hlchunk").setup({
+      chunk = {
+        enable = true
+      },
+      indent = {
+        enable = true
+      }
+    })
+  end
+}
+
--- vimfiles/nvim/lua/plugins/mini-indentscope.lua
+++
@@ -1,4 +0,0 @@
-return { 'echasnovski/mini.indentscope', version = false,
-config = true
-}
-
