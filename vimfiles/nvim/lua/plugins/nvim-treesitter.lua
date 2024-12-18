c18ca7701f01b4398244ecdad3c0a8dee2398331
mkiken <mkiken0@gmail.com>
2024-12-18 15:59:58 +0900

    [vim] nvim-treesitter-textobjects

--- vimfiles/nvim/lua/plugins/nvim-treesitter.lua
+++ vimfiles/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,4 +1,37 @@
 return {
   "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
-  config = true
+  config = true,
+  dependencies = {
+    {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      config = function()
+        require'nvim-treesitter.configs'.setup {
+          textobjects = {
+            select = {
+              enable = true,
+              lookahead = true,
+              keymaps = {
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner",
+              },
+            },
+            move = {
+              enable = true,
+              set_jumps = true,
+              goto_next_start = {
+                ["]m"] = "@function.outer",
+                ["]c"] = "@class.outer",
+              },
+              goto_previous_start = {
+                ["[m"] = "@function.outer",
+                ["[c"] = "@class.outer",
+              },
+            },
+          },
+        }
+      end
+    }
+  }
 }
