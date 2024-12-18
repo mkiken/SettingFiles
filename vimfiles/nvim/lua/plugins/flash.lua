048c60cfca9e25cdbfdf965823502946cc6e67b5
mkiken <mkiken0@gmail.com>
2024-12-18 14:13:25 +0900

    [vim] flash

---
+++ vimfiles/nvim/lua/plugins/flash.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {},
+  -- stylua: ignore
+  keys = {
+    { "<leader>e", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "<leader>E", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+  },
+}
