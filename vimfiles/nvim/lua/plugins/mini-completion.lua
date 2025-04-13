57e6472240ef91c0d05235a79e653661fd9c23bd
mkiken <mkiken0@gmail.com>
2025-04-14 01:38:13 +0900

    [vim] insert modeのC-fやC-bがmini.completionに取られていたので修正

--- vimfiles/nvim/lua/plugins/mini-completion.lua
+++ vimfiles/nvim/lua/plugins/mini-completion.lua
@@ -1,5 +1,16 @@
 return {
   'echasnovski/mini.completion',
   version = false,
-  config = true
+  config = -- No need to copy this inside `setup()`. Will be used automatically.
+{
+  -- Module mappings. Use `''` (empty string) to disable one. Some of them
+  -- might conflict with system mappings.
+  mappings = {
+    -- Scroll info/signature window down/up. When overriding, check for
+    -- conflicts with built-in keys for popup menu (like `<C-u>`/`<C-o>`
+    -- for 'completefunc'/'omnifunc' source function; or `<C-n>`/`<C-p>`).
+    scroll_down = '',
+    scroll_up = '',
+  },
+}
 }
