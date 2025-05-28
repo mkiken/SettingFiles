0d6d4df610644a67ab2d57ab553b6568cd62d7a6
mkiken <mkiken0@gmail.com>
2025-05-29 01:12:52 +0900

    [vim] noice追加

--- vimfiles/nvim/lua/plugins/lualine.lua
+++ vimfiles/nvim/lua/plugins/lualine.lua
@@ -9,7 +9,28 @@ return {
       sections = {
         lualine_c = {
           { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available }
-        }
+        },
+        lualine_x = {
+          {
+            require("noice").api.status.message.get_hl,
+            cond = require("noice").api.status.message.has,
+          },
+          {
+            require("noice").api.status.command.get,
+            cond = require("noice").api.status.command.has,
+            color = { fg = "#ff9e64" },
+          },
+          {
+            require("noice").api.status.mode.get,
+            cond = require("noice").api.status.mode.has,
+            color = { fg = "#ff9e64" },
+          },
+          {
+            require("noice").api.status.search.get,
+            cond = require("noice").api.status.search.has,
+            color = { fg = "#ff9e64" },
+          },
+        },
       }
     })
   end
