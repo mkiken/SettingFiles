027d14b98b50f8c6cd6c3da3a7e7a005712d4e3e
mkiken <mkiken0@gmail.com>
2025-05-29 01:32:45 +0900

    [vim] bufferlineのstyle調整

--- vimfiles/nvim/lua/plugins/bufferline.lua
+++ vimfiles/nvim/lua/plugins/bufferline.lua
@@ -1,3 +1,20 @@
 return {
   'akinsho/bufferline.nvim',
-  config = true }
+  config = function()
+    local bufferline = require('bufferline')
+    bufferline.setup {
+      options = {
+        style_preset = {
+          bufferline.style_preset.no_italic,
+          bufferline.style_preset.no_bold
+        },
+        hover = {
+          enabled = true,
+          delay = 200,
+          reveal = { 'close' }
+        },
+        diagnostics = "nvim_lsp",
+      }
+    }
+  end
+}
