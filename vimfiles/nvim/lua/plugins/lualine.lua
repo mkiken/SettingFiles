c93395e0d18d0e7b39c9706e1014bd4b58f88393
mkiken <mkiken0@gmail.com>
2025-05-28 23:46:41 +0900

    [vim] lualineにgit-blame表示

--- vimfiles/nvim/lua/plugins/lualine.lua
+++ vimfiles/nvim/lua/plugins/lualine.lua
@@ -1,9 +1,16 @@
+local git_blame = require('gitblame')
+
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('lualine').setup({
       options = { theme = 'onedark' },
+      sections = {
+        lualine_c = {
+          { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available }
+        }
+      }
     })
   end
 }
