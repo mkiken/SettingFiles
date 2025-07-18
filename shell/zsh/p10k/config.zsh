99eac524b90efc5938b865829a9ab8609a4bfd79
mkiken <mkiken0@gmail.com>
2025-07-19 04:01:55 +0900

    refactor(shell): .p10k.zshをリポジトリ側で管理

--- shell/zsh/p10k/config.zsh
+++ shell/zsh/p10k/config.zsh
@@ -6,4 +6,4 @@ source_and_zcompile_if_needed "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prom
 source_and_zcompile_if_needed "${BREW_PREFIX}/opt/powerlevel10k/share/powerlevel10k/powerlevel10k.zsh-theme"
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-source_and_zcompile_if_needed ~/.p10k.zsh
+source_and_zcompile_if_needed "${SET}shell/zsh/p10k/.p10k.zsh"
