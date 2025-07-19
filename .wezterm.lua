6567ca8e1cb3a4f2a68b5c55399503518cde0a26
mkiken <mkiken0@gmail.com>
2025-07-20 04:10:46 +0900

    refactor(wezterm): wezterm設定追加

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,17 +2,23 @@ local wezterm = require "wezterm"
 local config = wezterm.config_builder()
 local action = wezterm.action
 
-config.color_scheme = 'iceberg'
+config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = 'iceberg'
 config.font = wezterm.font_with_fallback {
   'SauceCodePro Nerd Font',
   'ヒラギノ角ゴシック',
   'Apple Color Emoji',
 }
-config.font_size = 12.0
+config.font_size = 12.5
 config.line_height = 1.0
 config.window_background_opacity = 0.85
+config.macos_window_background_blur = 5
 
 config.window_padding = { left = '0.5cell', right = '0.5cell', top = '0.5cell', bottom = '0.5cell' }
 config.default_cursor_style = 'BlinkingBar'
+
+config.enable_tab_bar = false
+
+config.notification_handling = "AlwaysShow"
 
 return config
