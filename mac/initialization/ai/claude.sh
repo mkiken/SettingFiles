dc4c1391c8f125bdacb02080759eda9eb0a4c2c2
mkiken <mkiken0@gmail.com>
2025-09-21 12:46:50 +0900

    refactor(claude): ♻️ claude-code-spec-workflowをグローバルインストールするようにする

--- mac/initialization/ai/claude.sh
+++ mac/initialization/ai/claude.sh
@@ -45,7 +45,7 @@ claude mcp add serena --scope "user" -- uvx --from git+https://github.com/oraios
 claude mcp add gitmcp -s user -- npx mcp-remote https://gitmcp.io/docs
 claude mcp add obsidian -s user -- npx -y mcp-obsidian "$HOME/Library/CloudStorage/Dropbox/Obsidian"
 
-claude-code-spec-workflow
+cd && claude-code-spec-workflow && cd $OLDPWD
 
 
 echo 'Claude setup and tools installation completed.'
