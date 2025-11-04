0a3322f4730153bb07a08c7f99b86b5f84905845
mkiken <mkiken0@gmail.com>
2025-11-04 16:47:14 +0900

    refactor(mac): ♻️ Roo CodeとCursorの初期設定削除

--- mac/initialization/ai/other.sh
+++ mac/initialization/ai/other.sh
@@ -8,10 +8,10 @@ echo "Setting up other AI assistants..."
 copy_if_not_exists "${Repo}ai/serena/serena_config.yml" ~/.serena/serena_config.yml
 
 # for Roo Code
-make_symlink "${Repo}ai/common/prompt.md" ~/.roo/rules/.roorules
+# make_symlink "${Repo}ai/common/prompt.md" ~/.roo/rules/.roorules
 
 # for Cursor
-make_symlink "${Repo}ai/common/mcp.json" ~/.cursor/mcp.json
+# make_symlink "${Repo}ai/common/mcp.json" ~/.cursor/mcp.json
 
 # for Spec Kit
 uv tool install specify-cli --from git+https://github.com/github/spec-kit.git
