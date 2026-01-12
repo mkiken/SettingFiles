43aacb8d163f8eebc1309d49a2a789de7ca388ca
mkiken <mkiken0@gmail.com>
2026-01-12 11:42:51 +0900

    feat(vimfiles): ✨ Vim/Neovimをquitするときに特殊ウィンドウを一気に閉じる

--- vimfiles/nvim/lua/api.lua
+++ vimfiles/nvim/lua/api.lua
@@ -17,3 +17,27 @@ vim.api.nvim_create_autocmd('TextYankPost', {
         vim.highlight.on_yank({ higroup = 'Visual', timeout = 300 })
     end,
 })
+
+-- [Vim/Neovimをquitするときに特殊ウィンドウを一気に閉じる](https://zenn.dev/vim_jp/articles/ff6cd224fab0c7)
+vim.api.nvim_create_autocmd('QuitPre', {
+  callback = function()
+    -- 現在のウィンドウ番号を取得
+    local current_win = vim.api.nvim_get_current_win()
+    -- すべてのウィンドウをループして調べる
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      -- カレント以外を調査
+      if win ~= current_win then
+        local buf = vim.api.nvim_win_get_buf(win)
+        -- buftypeが空文字（通常のバッファ）があればループ終了
+        if vim.bo[buf].buftype == '' then
+          return
+        end
+      end
+    end
+    -- ここまで来たらカレント以外がすべて特殊ウィンドウということなので
+    -- カレント以外をすべて閉じる
+    vim.cmd.only({ bang = true })
+    -- この後、ウィンドウ1つの状態でquitが実行されるので、Vimが終了する
+  end,
+  desc = 'Close all special buffers and quit Neovim',
+})
