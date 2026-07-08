d7103c9325aa18f185cc499619a14773ac72bedb
mkiken <mkiken0@gmail.com>
2026-07-09 00:21:10 +0900

    fix(vimfiles): 🐛 スクロールバーを透過する

--- vimfiles/nvim/lua/plugins/nvim-scrollbar.lua
+++ vimfiles/nvim/lua/plugins/nvim-scrollbar.lua
@@ -1,6 +1,10 @@
 return {
   'petertriho/nvim-scrollbar',
   opts = {
+    handle = {
+      color = "#9399b2",
+      blend = 60,
+    },
     handlers = {
       -- trueにすると文字列の一番右の文字が見えなくなることがある
       cursor = false,
