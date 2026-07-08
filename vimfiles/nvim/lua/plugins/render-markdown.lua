3455904e94671aa825008a8a866fc75bdd394c75
mkiken <mkiken0@gmail.com>
2026-07-09 00:35:09 +0900

    feat(vimfiles): ✨ markdownプレビューを追加

---
+++ vimfiles/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,28 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  ft = { "markdown" },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    enabled = true,
+    render_modes = { "n", "i", "c" },
+    file_types = { "markdown" },
+    sign = {
+      enabled = false,
+    },
+    heading = {
+      icons = { "# ", "## ", "### ", "#### ", "##### ", "###### " },
+    },
+    completions = {
+      lsp = {
+        enabled = true,
+      },
+    },
+  },
+  keys = {
+    { "<Leader>mp", "<cmd>RenderMarkdown toggle<CR>", desc = "Markdown previewをトグルする" },
+    { "<Leader>mP", "<cmd>RenderMarkdown preview<CR>", desc = "Markdown previewを横に開く" },
+  },
+}
