19ea72aa921226dd5396a506e294d010ced8a7a4
mkiken <mkiken0@gmail.com>
2026-07-11 00:43:07 +0900

    refactor(shell): ♻️ こないケースの通知削除

--- shell/zsh/alias/utils.zsh
+++ shell/zsh/alias/utils.zsh
@@ -493,9 +493,6 @@ function _start_prompt_wait_notification() {
             _title="入力待ち"
         fi
         notify --tmux-icon "${EMOJI_STATUS_NOTIFICATION:-✋}" "$_title" "$message" "default" "$group" || true
-    elif (( ${+functions[update_tmux_window_name]} )); then
-        # notify が読み込めない環境でもアイコンだけは従来どおり出す
-        update_tmux_window_name "${EMOJI_STATUS_NOTIFICATION:-✋}" || true
     fi
 
     return 0
